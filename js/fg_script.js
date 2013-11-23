60ebf8292d0cd5b2387f5669e38189a49342548c
PankajAgarwal <PankajAgarwal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-23 08:35:28 +0000

    add 1.0 folder
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-integrated-with-google-maps/tags/1.0@809223 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/fg_script.js
@@ -0,0 +1,64 @@
+jQuery(document).ready(function() {
+    jQuery('.fg_group').hide();
+    jQuery('.fg_group:first').fadeIn();
+    jQuery('#fg_of-nav li:first').addClass('current');
+    jQuery('#fg_of-nav li a').click(function(evt) {
+        jQuery('#fg_of-nav li').removeClass('current');
+        jQuery(this).parent().addClass('current');
+        if (jQuery(this).attr("title") == "Embed Code") {
+            jQuery(".fg_embed_code_save").css("display", "block");
+            jQuery(".fg_embed_code_save").css("float", "left");
+            jQuery(".fg_embed_code_save").css("width", "120px");
+            jQuery(".fg_embed_code_save").css("height", "40px");
+        }
+        else {
+            jQuery(".fg_embed_code_save").css("display", "none");
+        }
+        var clicked_group = jQuery(this).attr('href');
+        jQuery('.fg_group').hide();
+        jQuery(clicked_group).fadeIn();
+        evt.preventDefault();
+    });
+    jQuery('.fg_embed_code_save').click(function() {
+        jQuery('div#loader_img').css("display", "block");
+        var text_value = jQuery('textarea#fg_content_html').val();
+        var bg_color = jQuery('input.color').val();
+        var googlemap_code = jQuery('textarea.googlemap_iframe').val();
+        var latitude_map = jQuery('input.latitude_value').val();
+        var longitude_map = jQuery('input.longitude_value').val();
+        var location_address = jQuery('textarea.address').val();
+        if (text_value == "") {
+            alert("code textarea is empty");
+            jQuery('div#loader_img').css("display", "none");
+        }
+        else {
+            var pattern = /iframe/;
+            var exists = pattern.test(text_value);
+            if (exists) {
+                var data = {
+                    action: 'master_response',
+                    value: text_value,
+                    color: bg_color,
+                    iframe: googlemap_code,
+                    latitude: latitude_map,
+                    longitude: longitude_map,
+                    address: location_address
+                };
+                jQuery.post(script_call.ajaxurl, data, function(response) {
+                    if (response) {
+                        //alert(response);
+                        jQuery('div#loader_img').css("display", "none");
+                    }
+                    else {
+                        //alert("error");
+                        jQuery('div#loader_img').css("display", "none");
+                    }
+                });
+            }
+            if (!exists) {
+                alert("Plz paste the correct code");
+                jQuery('div#loader_img').css("display", "none");
+            }
+        }
+    });
+});
